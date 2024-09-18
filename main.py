a5e3aa67b20c6d5f79acb68f326f629a90d90049
Brian Hernandez <ricardo.hernandez@rightbalance.io>
2024-09-18 03:15:30 -0600

    prompt templates training

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-# RAG workflow is as follows:
-# 1. Read the input from the user
-# 2. Preprocess the input
-# 3. Generate the answer using RAG
-# 4. Print the answer
-# 5. Generate the next question
-# 6. Repeat the process
+import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
 
+load_dotenv()
+
+template = """Te voy a dar informacion sobre algunas bicicletas, me tienes que dar
+la informacion (en español) del modelo, marca, precio, tipo y descripcion de las
+primeras 3 que aparezcan de forma estructurada (si aparecen menos, me das las que aparezcan).
+{response}"""
+
+prompt_template = PromptTemplate(
+  input_variables=["respuesta"], template=template
+)
+
+llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo", max_tokens=1000)
+
+prompt = PromptTemplate.from_template("Dime que soy {adjetivo} y por que lo soy")
+
+print(prompt.format(adjetivo="inteligente"))
