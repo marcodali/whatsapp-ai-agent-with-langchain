af9f80caf54d1248b26753eebbe95457e5306831
Brian Hernandez <ricardo.hernandez@rightbalance.io>
2024-09-18 22:41:33 -0600

    Funciono bien sin warnings. A partir de ahora se usara RunnableSequence en vez de LLMChain

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
-from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
+from langchain.schema.runnable import RunnableSequence
 
 load_dotenv()
 
-# Translator example
+# Crear un traductor de cualquier idioma a cualquier idioma
 traductor_template = "Eres un asistente útil que traduce del {idioma_origen} al {idioma_destino} el texto: {texto}."
 traductor_prompt_template = PromptTemplate(
     input_variables=["idioma_origen", "idioma_destino", "texto"],
@@ -13,8 +13,13 @@ traductor_prompt_template = PromptTemplate(
 )
 
 llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-chain = LLMChain(llm=llm, prompt=traductor_prompt_template)
+
+# Crear una secuencia ejecutable
+chain = RunnableSequence(
+    traductor_prompt_template,
+    llm
+)
 
 texto = "Donde nuestras voces suenan ven a buscarnos, nos hemos llevado lo que mas deseas y para encontrarlo tienes una hora."
 respuesta = chain.invoke({"idioma_origen": "español", "idioma_destino": "inglés", "texto": texto})
-print(respuesta['text'])
+print(respuesta.content)
